a6f8af8a5b17ece2a938a407b1d19e15f3c07422
holaroman <aroman22@scienceleadership.org>
2021-03-08 13:35:23 -0500

    hwloooo

--- src/sla/org/Main.java
+++ src/sla/org/Main.java
@@ -14,10 +14,17 @@ import javafx.scene.control.ChoiceBox;
 
 public class Main  extends Application{
 
-        void clicked (){
+        ImageView imagesView;
+
+        Image image2;
+        Image image3;
+        Image image4;
+        Image image5;
+
+        ChoiceBox choiceBox;
 
 
-        }
+
 
         @Override
         public void start(Stage primaryStage) throws Exception {
@@ -28,30 +35,33 @@ public class Main  extends Application{
 
 
             FileInputStream input2 = new FileInputStream("/Users/aidanroman/Desktop/Classified.jpeg");
-            Image image2 = new Image(input2);
+            image2 = new Image(input2);
 
             FileInputStream input3 = new FileInputStream("/Users/aidanroman/Desktop/images.jpeg");
-            Image image3 = new Image(input3);
+            image3 = new Image(input3);
 
             FileInputStream input4 = new FileInputStream("/Users/aidanroman/Desktop/images-1.jpeg");
-            Image image4 = new Image(input4);
+            image4 = new Image(input4);
 
 
             FileInputStream input5 = new FileInputStream("/Users/aidanroman/Desktop/images-2.jpeg");
-            Image image5 = new Image(input5);
+            image5 = new Image(input5);
 
-            ImageView imageView = new ImageView(image);
+            imagesView = new ImageView(image);
 
-            ChoiceBox choiceBox = new ChoiceBox();
+            choiceBox = new ChoiceBox();
 
             choiceBox.getItems().add("Classified documents");
             choiceBox.getItems().add("Walt Disney's arm");
             choiceBox.getItems().add("Prized jewels");
             choiceBox.getItems().add("dave");
+            choiceBox.setOnAction(actionEvent -> {
 
+                choiceboxUsed();
+            });
         ;
 
-            VBox layout = new VBox(imageView,choiceBox);
+            VBox layout = new VBox(imagesView,choiceBox);
             Scene scene = new Scene(layout, 600, 400);
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -59,6 +69,11 @@ public class Main  extends Application{
 
 
             primaryStage.show();
+        }
+
+        void choiceboxUsed(){
+    String Choice = choiceBox.getId();
+            imagesView.setImage(image2);
         }
 
          public static void main(String[] args) {
